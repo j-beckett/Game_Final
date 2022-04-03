26b3884e4c520ccbb827827d256e6ec0b9b32377
J Beckett <jennybeckett2@hotmail.com>
2022-04-03 14:25:03 -0600

    first

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    private float zMovement = 0f; //even though movement comes from up/down keys we actually want to modify where the player is on the Z plane
+
+    private float xMovement = 0f;
+
+    [SerializeField] private float speed = 5f;
+
+    
+    private CharacterController cc;
+
+    private float gravity = -9.81f;
+    private float yVelocity = 0.0f;
+
+    //private float yVelocityWhenGrounded = -4.0f;
+
+
+    // jumping vars
+
+    private float time = 0.0f;
+    private bool isMoving = false;
+    private bool jumpPress = false;
+
+    //lab 10 
+
+    private float pushForce = 5.0f;
+
+    //flashlight controls 
+
+    [SerializeField] Light playerFlashLight;
+    private bool lightEnabled = false;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody; 
+
+        // does it have a rigidbody and is Physics enabled?
+        if (body != null && !body.isKinematic) {
+            body.velocity = hit.moveDirection * pushForce;
+        } 
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal"); 
+        float deltaZ = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+        // Clamp magnitude for diagonal movement
+        movement = Vector3.ClampMagnitude (movement, 1.0f);
+
+        // determine how far to move on the XZ plane movement *= speed;
+        movement *= speed;
+
+        //gravity is here
+        yVelocity += gravity * Time.deltaTime;
+
+        if (cc.isGrounded && yVelocity < 0.0)
+        {
+            //yVelocity = yVelocityWhenGrounded;
+        }
+
+        movement.y = gravity;
+
+        //end gravity
+
+        // Movement code Frame Rate Independent 
+        movement *= Time.deltaTime;
+
+
+        // Convert local to global coordinates 
+        movement = transform.TransformDirection(movement);
+        cc.Move(movement);
+
+
+        jumpPress = Input.GetButtonDown("Jump");
+
+        if (jumpPress)
+        {
+            transform.Translate(Vector3.up, Space.World);
+        }
+
+        //Flashlight controller 
+
+        if (Input.GetButtonDown("Flashlight"))
+        {
+            lightEnabled = !lightEnabled;
+            playerFlashLight.enabled = lightEnabled;
+        }
+    }
+
+}
